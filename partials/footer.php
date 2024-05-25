a508bbcd73372578922022378e874e30d57bec5d
kylencopilot <kinner.leighann499@hotmail.com>
2024-05-25 21:26:48 +0700

    first commit

---
+++ partials/footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy; Created With Loved By Adityawarman
+            </div>
+
+        </div>
+    </div>
+</footer>
+<!-- FOOTER SECTION END-->
+<!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+<!-- CORE JQUERY SCRIPTS -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- BOOTSTRAP SCRIPTS  -->
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
